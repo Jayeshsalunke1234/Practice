e6539e511a6453923e2b751082fa565871589dd4
Jayeshsalunke1234 <103381682+Jayeshsalunke1234@users.noreply.github.com>
2025-04-17 11:33:08 +0530

    Update order.java

--- order.java
+++ order.java
@@ -2,6 +2,13 @@
 class Mainu{
     public static void main(String []args){
         System.out.println("Ordering the food");
-    
+
+        List l=new ArrayList();
+        l.add("butter chicken");
+        l.add("momos");
+        l.add("chicken curry");
+
+        System.out.println(l);
+        
         }
 }
